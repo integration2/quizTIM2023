6f0b150b59525b945f4db3d4fffa033d4f2b7166
Félix Vermette <captainfilstudio@gmail.com>
2023-04-28 12:41:30 -0400

    ajout quiz captainfil

---
+++ captainfil/README.md
@@ -0,0 +1,10 @@
+# Le lapin domestique
+
+le squellette du projet qui inclus le HTML est le css est presque terminé et le javascipt est commencer
+
+## TO-DO
+ * ajuster le css pour tout les formats (fond hover) !done
+ * vérifier typo !done
+
+## Conclu
+le travail avance bien
---
+++ captainfil/assets/js/app.js
@@ -0,0 +1,136 @@
+/**
+ * TIM - QUIZ
+ * @author: Prénom Nom;
+ **/
+
+/* Données du quiz - À ADAPTER SELON LES CONTENUS DU QUIZ */
+/* CONSTRUIRE le tableau des bonneRéponses avec les identifiants 
+des input[type=radio] */
+document.querySelector('form').addEventListener('submit', empecherEnvoiForm);
+
+function empecherEnvoiForm(objEvenement) {
+    objEvenement.preventDefault();
+}
+
+
+const objJSON = {
+    "retroactions": {
+        "positive": "Bonne réponse!",
+        "negative": "Oops! Mauvaise réponse"
+    },
+    "explications": {
+        "Q1": "Quand un lapin est stressé, il tape de la patte arrière afin de créer un bruit fort. Cela arrive surtout lorsqu’il y un changement dans son environnement ou lorsqu’il se sent directement menacé. Parfois, un simple bruit un peu trop fort comme quelqu’un qui éternue ou les voisins qui font des travaux peut provoquer un lapin qui tape de la patte",
+        "Q2": "Le lapin blanc d'hotot est reconnaissable à sa couleur blanche et la marque noire autour de ses yeux. Il a rarement les oreilles qui tombent, un trait communément appelé  bélier ou “lop”. Il peut parfois être difficile d’identifier la race de son lapin et cela peut devenir encore plus compliqué avec les races mélangées.",
+        "Q3": "Bien que tous ces choix sont tous appréciés des lapins, rien ne les compare à une banane. Il est important de se rappeler qu’une banane contient beaucoup de sucre et peut si consommé en grande quantité, être dangereux pour la santé du lapin. Il faut donc ne pas trop en donner. comme en général les lapins adorent les bananes si vous voulez faire plaisir a votre compagnon vous pouvez la partager avec lui. Il est important de noter aussi que les carottes contiennent elles aussi beaucoup de sucre et doivent être aussi donné en quantité à limité. La laitue et les concombres sont totalement sécuritaires et font un excellent accompagnement pour le granulé et le foin que le lapin devrait déjà avoir."
+    },
+    "bonnesReponses": [
+        "QA1",
+        "QB3",
+        "QC2"
+    ],
+    "messages": {
+        "resultatsDebut": "Vous avez obtenu un résultat de",
+        "note0": "Vous auriez pu faire mieux. Je vous suggère ..",
+        "note33": "Vous auriez pu faire mieux. Je vous suggère ..",
+        "note66": "Bravo, vous avez une bonne connaissance générale de ...",
+        "note100": "Félicitations, vous êtes un fin connaisseur !"
+    }
+};
+
+document.addEventListener('DOMContentLoaded', function(){
+    document.querySelector('body').classList = "js"
+    quiz.initierQuiz();
+})
+
+document.querySelectorAll('.radio').forEach(function(item){
+    item.addEventListener('click', function(e){ 
+            if( e.currentTarget.name != "Q2"){
+                 e.currentTarget.closest("div").classList = "question__box-disabeld"
+                 e.currentTarget.closest('fieldset').querySelector ;
+            }
+        selectionBouton(e)
+    })
+})
+
+
+function selectionBouton(e) {
+    document.querySelectorAll('.question__box-disabeld').forEach(function(item){ item.classList = "question__box" });
+    
+
+    if( e.currentTarget.name != "Q2"){
+    e.currentTarget.closest("div").classList = "question__box-disabeld"
+    }
+
+    document.querySelector('.ctnButton .bouton').classList.remove('disabled');
+    document.querySelector('.ctnButton .bouton').disabled = false ;
+}
+
+
+/* Objet Quiz */
+const quiz = {
+    intQuestion: 0 ,
+    intBonneReponse: 0,
+    initierQuiz: function() { 
+        document.querySelector('.quiz').classList.add("hidden")
+        document.getElementById("resultat").classList.add("hidden")
+        document.querySelectorAll('fieldset').forEach(function(item){
+            item.classList = "hidden";
+        });
+
+        const refBtnStart = document.createElement('button');
+        refBtnStart.innerHTML = 'Commencer <img src="assets/images/arrow.svg" class="arrow" alt="">';
+        refBtnStart.classList = "bouton";
+        refBtnStart.addEventListener('click', this.debuterQuiz.bind(this) )
+        document.querySelector('.text__intro').append(refBtnStart)
+
+        document.getElementById("mainButton").addEventListener('click' , this.validerReponse.bind(this))
+    },
+
+    debuterQuiz: function () {
+        document.getElementById('introduction').classList = "hidden"
+
+        document.getElementById("Q1").classList = ""
+        document.querySelector('.quiz').classList.remove("hidden")
+     },
+     
+    validerReponse: function () {
+        let strReponse = document.querySelector('.radio:checked').id
+
+        if(strReponse == objJSON.bonnesReponses[this.intQuestion]){
+            this.intBonneReponse++
+        }
+
+        document.getElementById("Q" + (this.intQuestion + 1)).classList = "hidden"
+        if(this.intQuestion < 2){
+        document.getElementById("Q" + (this.intQuestion + 2)).classList = ""
+        } else {
+            this.afficherResultats()
+        }
+
+        document.querySelector('.radio:checked').checked = false;
+        this.intQuestion++
+        document.querySelector('.ctnButton .bouton').classList.add("disabled")
+     },
+    afficherResultats: function () {
+        document.getElementById("mainButton").classList.add("hidden")
+        document.getElementById("resultat").classList.remove("hidden")
+
+        document.getElementById("feedback").innerHTML = "Votre résultat est : </br>" + this.intBonneReponse + "/3";
+     },
+     rejouer: function(){
+        quiz.intBonneReponse = 0;
+        quiz.intQuestion = 0;
+        document.querySelector('.quiz').classList.add("hidden")
+        document.getElementById("resultat").classList.add("hidden")
+        document.querySelectorAll('fieldset').forEach(function(item){
+            item.classList = "hidden";
+        });
+        document.getElementById("mainButton").classList.remove("hidden")
+        document.getElementById("resultat").classList.add("hidden")
+     }
+}
+
+document.getElementById("resultat").addEventListener("click", quiz.rejouer());
+
+
+
